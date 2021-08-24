15433b6387b29d682a757cb28e84021a2d271cef
yeg2799 <yeg2799@gmail.com>
2021-08-25 00:10:37 +0300

    login page form ui design

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -5,7 +5,20 @@
       </div>
       <div class="login_page_right">
         <div class="login_page_right_box">
-
+          <div class="login_page_right_box_form">
+              <form>
+                <div class="mb-3">
+                  <input type="email" class="form-control" placeholder="Lütfen E-Posta adresinizi giriniz">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                  <input type="password" class="form-control" placeholder="Lütfen şifrenizi giriniz">
+                </div>
+                <button type="submit" class="btn">Giriş Yap</button>
+                <nuxt-link to="/forget-password">Şifreni mi unuttun</nuxt-link>
+                <button type="submit" class="btn create_account_button"> Yeni Hesap Oluştur</button>
+              </form>
+          </div>
         </div>
       </div>
   </div>
@@ -33,11 +46,28 @@ export default {
     align-items: center;
     justify-content: center;
     &_box{
-      width: 500px;
-      height: 500px;
+      width: 400px;
+      height: 300px;
       background: #EFEFEF;
       border: 1px solid #EFEFEF;
       border-radius: 10px;
+      &_form{
+        padding: 20px;
+        input{
+          font-size: 14px;
+        }
+        button{
+          width: 100%;
+          background: #CEBE79;
+          border-color: #CEBE79;
+          color: #fff;
+        }
+        .create_account_button{
+          background: #EFEFEF;
+          color: #CEBE79;
+          margin-top: 20px;
+        }
+      }
     }
   }
 }
