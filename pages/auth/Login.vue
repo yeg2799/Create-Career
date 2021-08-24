b1fa1849fa58523897201828ae75fdbf646150d6
yeg2799 <yeg2799@gmail.com>
2021-08-24 23:50:47 +0300

    created task for login page

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-      Giriş Sayfası
+  <div class="login_page">
+      <div class="login_page_left">
+        <img src="@/assets/images/login.png" alt="">
+      </div>
+      <div class="login_page_right">
+        <div class="login_page_right_box">
+
+        </div>
+      </div>
   </div>
 </template>
 
@@ -10,6 +17,28 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.login_page{
+  display: flex;
+  &_left{
+    width: 50%;
+    flex: 1;
+    img{
+      width: 100%;
+    }
+  }
+  &_right{
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &_box{
+      width: 500px;
+      height: 500px;
+      background: #EFEFEF;
+      border: 1px solid #EFEFEF;
+      border-radius: 10px;
+    }
+  }
+}
 </style>
